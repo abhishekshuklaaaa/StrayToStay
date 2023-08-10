f6adfea97e464d761cc84153a6ed9390c98af854
abhishekshuklaaaa <201617@kit.ac.in>
2023-08-10 23:36:03 +0530

    hi

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,23 +15,23 @@ const Hero = () => {
       <div className='absolute w-full h-full top-0 left-0 bg-gray-900/30'></div>
       <div className='absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4'>
         <h1>Rescue, Rehabilitate, Rehome</h1>
-        <h2 className='py-4'>Changing Lives Fur-ever!</h2>
+        <h2 className='py-4'> Changing Lives Fur-ever!</h2>
         <form
           className='flex justify-between items-center max-w-[700px] mx-auto w-full border p-1
           rounded-md text-black bg-gray-100/90'
         >
           <div>
             <input
-              className='bg-transparent w-[300px] sm:w-[400px] font-[Poppins] focus:outline-none'
+              className='bg-transparent w-[300px] sm:w-[400px] font-[Poppins] focus:outline-none
+                  '
               type='text'
               placeholder='Find Your Paw-some Companion'
             />
           </div>
-          <div className='flex items-center'>
-            <button className='mr-2 sm:mr-4'>
-              <AiOutlineSearch size={20} className='icon' style={{ color: '#ffffff' }} />
+          <div>
+            <button>
+              <AiOutlineSearch size={20} className='icon' style={{color: '#ffffff'}}  />
             </button>
-            <span className='hidden sm:inline-block'>Search</span>
           </div>
         </form>
       </div>
