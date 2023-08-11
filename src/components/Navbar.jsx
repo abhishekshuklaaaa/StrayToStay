a2675f594cd174bc376d3631bfda4b1e5b154018
abhishekshuklaaaa <201617@kit.ac.in>
2023-08-11 16:33:59 +0530

    hyyyyyy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,51 +3,73 @@ import { BsPerson } from 'react-icons/bs';
 import { BiSearch } from 'react-icons/bi';
 import { AiOutlineClose } from 'react-icons/ai';
 import { HiOutlineMenuAlt4 } from 'react-icons/hi';
+import { FaMusic } from 'react-icons/fa'; // Import the music icon
+import bgAudio from './music.mp3.mp3'
 
 import './Navbar.css'; // Import the CSS file
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const [sticky, setSticky] = useState(false);
+  const [musicPlaying, setMusicPlaying] = useState(false); // State to track music playing
 
   const handleNav = () => {
     setNav(!nav);
   };
 
+  const toggleMusic = () => {
+    setMusicPlaying(!musicPlaying);
+  };
+
   useEffect(() => {
+    let audio = new Audio(bgAudio);
+
+    if (musicPlaying) {
+      audio.play();
+      audio.loop = true;
+    } else {
+      audio.pause();
+      audio.currentTime = 0; // Reset the audio to the beginning
+    }
+
     const handleScroll = () => {
-      if (window.scrollY > 50) {
-        setSticky(true);
-      } else {
-        setSticky(false);
-      }
+      // Your scroll handling logic here
+    };
+
+    const handleVisibilityChange = () => {
+      // Your visibility change handling logic here
     };
 
     window.addEventListener('scroll', handleScroll);
+    document.addEventListener('visibilitychange', handleVisibilityChange);
+
     return () => {
+      audio.pause(); // Pause the audio when the component unmounts
       window.removeEventListener('scroll', handleScroll);
+      document.removeEventListener('visibilitychange', handleVisibilityChange);
     };
-  }, []);
+  }, [musicPlaying]);
 
   return (
     <div className={`flex w-full justify-between items-center h-20 px-4 absolute z-10 ${sticky ? 'sticky' : ''} ${nav ? 'hidden' : ''}`}>
-      <div>
-        <h1 className='cursor-pointer text-3xl md:text-4xl font-bold' onClick={() => window.location.reload()}>StrayToStay.</h1> {/* Adding the website name */}
+    <div className='cursor-pointer text-3xl md:text-4xl font-bold flex items-center'>
+      StrayToStay
+      <button className="music-button" onClick={toggleMusic}>
+        <FaMusic size={18} className={`${musicPlaying ? 'playing' : ''}`} />
+        </button>
       </div>
-      <ul className='hidden md:flex'>
+      <ul className={`hidden md:flex ${nav ? '' : 'md:flex'}`}>
         <li>🏠 Home</li>
         <li>🛠️ Services</li>
-        {/* Add other components */}
         <li>🌟 About Us</li>
         <li>💌 Contact</li>
       </ul>
-      <div className='hidden md:flex'>
-        <BiSearch size={20} />
-        <BsPerson size={20} />
+      <div className='md:hidden'>
+        <HiOutlineMenuAlt4 size={20} onClick={handleNav} />
       </div>
-      {/* ... rest of your code */}
     </div>
   );
 };
+ 
 
 export default Navbar;
